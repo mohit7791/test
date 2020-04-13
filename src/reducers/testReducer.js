827a51aead7034cdcc78e282f8253d9eed79a34a
Mohit <mohitwav@gmail.com>
2020-04-13 15:15:49 +0530

    Second commit

--- src/reducers/testReducer.js
+++ src/reducers/testReducer.js
@@ -1,9 +1,7 @@
 const iniState={user:[]}
 const testReducer=(state=iniState,action)=>{
 switch(action.type){
-
         case "GET_POST":
-
             return{
             ...state,
             user:state.user.concat(action.payload)
