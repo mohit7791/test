5beadea0c14589154ce2d53ce4349c8e92f38726
Mohit <mohitwav@gmail.com>
2020-04-13 16:36:37 +0530

    Chnaged

--- backend/client/src/try.js
+++ backend/client/src/try.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 export const  te=(data)=>async(dispatch)=>{
-    await axios.post('http://localhost:4000/todos/add', data)
+    await axios.post('http://localhost:8000/todos/add', data)
     .then(res => console.log(res.data));
-    await axios.get('http://localhost:4000/todos')
+    await axios.get('http://localhost:8000/todos')
     .then(res => dispatch({type:"GET_POST",payload:res.data}));
     
 }
